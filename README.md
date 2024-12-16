1c1a4baafefe35855730b91539e2fca793f56039
José Tomaz <rbj.ribeiro@gmail.com>
2024-12-16 13:21:21 +0000

    adiciona scripts `lint:check` e lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Apenas treino
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Jaque, eu amo você. Se você me ama, dá uma risadinha! ❤💖</h1>
+  return <h1>Jaque, eu amo você. Se você me ama, dá uma risadinha! ❤💖</h1>;
 }
 
-export default Home
+export default Home;
